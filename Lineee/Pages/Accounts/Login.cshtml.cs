308e9a5ff4d1179748aa725e5b774aa0c80fc649
skojima-ii-kyoto-u <skojima@ii.ist.i.kyoto-u.ac.jp>
2018-06-17 21:03:33 +0900

    make login and talks

--- Lineee/Pages/Accounts/Login.cshtml.cs
+++ Lineee/Pages/Accounts/Login.cshtml.cs
@@ -37,16 +37,15 @@ namespace Lineee.Pages.Accounts
 
             if (target != null)
             {
-                Console.Error.WriteLine(target.ID);
-                Console.Error.WriteLine(target.Name);
-                return Page();
+                return RedirectToPage("../Talks/All", new { id = target.ID });
             }
             else
             {
                 _context.Account.Add(Account);
                 await _context.SaveChangesAsync();
 
-                return RedirectToPage("./Index");
+                target = await _context.Account.SingleOrDefaultAsync(m => m.Name == Account.Name);
+                return RedirectToPage("../Talks/All", new { id = target.ID });
             }
         }
     }
---
+++ Lineee/Pages/Talks/All.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lineee.Models;
+
+namespace Lineee.Pages.Talks
+{
+    public class AllModel : PageModel
+    {
+        private readonly AccountContext _accContext;
+
+        public IList<Account> AccountList { get; set; }
+        public string MyName { get; set; }
+
+        public AllModel(AccountContext accountContext)
+        {
+            _accContext = accountContext;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task OnGetAsync(int id)
+        {
+            MyName = (await _accContext.Account.FindAsync(id)).Name;
+            AccountList = await _accContext.Account.ToListAsync();
+        }
+    }
+}
