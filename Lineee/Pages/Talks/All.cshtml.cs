c8132e129db858d2cb49ff14fd602a94690005bb
skojima-ii-kyoto-u <skojima@ii.ist.i.kyoto-u.ac.jp>
2018-06-20 00:18:02 +0900

    submit version

--- Lineee/Pages/Talks/All.cshtml.cs
+++ Lineee/Pages/Talks/All.cshtml.cs
@@ -12,20 +12,35 @@ namespace Lineee.Pages.Talks
     public class AllModel : PageModel
     {
         private readonly AccountContext _accContext;
+        private readonly TalkContext _talkContext;
 
         public IList<Account> AccountList { get; set; }
-        public string MyName { get; set; }
+        public Account MyAccount { get; set; }
+        public Dictionary<Account, string> LastTalk { get; set; }
 
-        public AllModel(AccountContext accountContext)
+        private int myid;
+
+        public AllModel(AccountContext accountContext, TalkContext talkContext)
         {
             _accContext = accountContext;
+            _talkContext = talkContext;
         }
         
         [HttpGet("{id}")]
         public async Task OnGetAsync(int id)
         {
-            MyName = (await _accContext.Account.FindAsync(id)).Name;
+            myid = id;
+            MyAccount = await _accContext.Account.FindAsync(id);
             AccountList = await _accContext.Account.ToListAsync();
+            LastTalk = new Dictionary<Account, string>();
+            //register last post to "LastTalk"
+            foreach(Account acc in AccountList)
+            {
+                var lt = await _talkContext.Talk.LastOrDefaultAsync(talk => 
+                    (talk.FromNameID == myid && talk.ToNameID == acc.ID) || 
+                    (talk.ToNameID == myid && talk.FromNameID == acc.ID) );
+                LastTalk.Add(acc, (lt != null) ? lt.Message : "（まだメッセージがありません）");
+            }
         }
     }
 }
--- Lineee/Pages/Talks/Room.cshtml.cs
+++ Lineee/Pages/Talks/Room.cshtml.cs
@@ -4,14 +4,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lineee.Models;
 
 namespace Lineee.Pages.Talks
 {
     public class RoomModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AccountContext _accContext;
+        private readonly TalkContext _talkContext;
 
+        //public IList<Talk> TalkList { get; set; }
+        public DbSet<Talk> Talks { get; set; }
+        public Account MyAccount { get; set; }
+        public Account ToAccount { get; set; }
+        
+        [BindProperty]
+        public Talk NewTalk { get; set; }
+
+        public RoomModel(AccountContext accountContext, TalkContext talkContext)
+        {
+            _accContext = accountContext;
+            _talkContext = talkContext;
+        }
+
+        [HttpGet("{myid}/{toid}")]
+        public async Task OnGetAsync(int myid, int toid)
+        {
+            MyAccount = await _accContext.Account.FindAsync(myid);
+            ToAccount = await _accContext.Account.FindAsync(toid);
+            Talks = _talkContext.Talk;
+        }
+
+        public async Task<IActionResult> OnPostAsync(int myid, int toid)
+        {
+            await _talkContext.AddAsync(new Talk()
+            {
+                FromNameID = myid,
+                ToNameID = toid,
+                Message = NewTalk.Message,
+                DateTime = DateTime.Now
+            });
+            
+            await _talkContext.SaveChangesAsync();
+            NewTalk.Message = "";
+            return RedirectToPage("./Room", new { myid = myid, toid = toid });
         }
     }
 }
